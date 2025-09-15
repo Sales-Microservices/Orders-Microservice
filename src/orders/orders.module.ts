8ac9b278680c521c22dda87d4ecfcc1441d8fc87
Bernapai <juanemeunier@gmail.com>
2025-09-15 19:13:55 -0300

    Add Prisma Config + Schema

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { OrdersService } from './orders.service';
-import { OrdersController } from './orders.controller';
+import { OrdersService } from '../orders/services/orders.service';
+import { OrdersController } from '../orders/controllers/orders.controller';
 
 @Module({
   controllers: [OrdersController],
   providers: [OrdersService],
 })
-export class OrdersModule {}
+export class OrdersModule { }
